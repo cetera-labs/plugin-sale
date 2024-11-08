7425337573dfced77de26ca034d88f485256fa1d
v.eremeev <vladimirt345@gmail.com>
2024-11-08 12:42:41 +0300

    Возможность получения активной скидки для класса Offer

--- classes/Sale/Offer.php
+++ classes/Sale/Offer.php
@@ -21,5 +21,16 @@ class Offer extends Buyable
 		}
 		return round($value);
 	}
-	
+	public function getActiveDiscounts($field = 'price') {
+		if (!$this->product) {
+			$value = 0;
+		}
+		else {
+			$value = $this->product->getDiscount($field, $this->getFullPrice($field), $this->isInCart, $this);
+		}
+		if ($this->product->activeDiscounts === null) {
+			$this->product->getDiscount();
+		}
+		return $this->product->activeDiscounts;
+	}
 }
