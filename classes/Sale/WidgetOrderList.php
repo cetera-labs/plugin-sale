75e63ae78a4bcbb3fa33dc8ebfe660de1daaf14f
v.eremeev <vladimirt345@gmail.com>
2025-08-11 16:26:00 +0300

    MTRMC-2315 - вывел id товаров в массив

--- classes/Sale/WidgetOrderList.php
+++ classes/Sale/WidgetOrderList.php
@@ -22,15 +22,44 @@ class WidgetOrderList extends \Cetera\Widget\Templateable
 		$this->application->addScript('/cms/plugins/sale/js/common.js');		
 	}	
 
-	public function getOrders()
-	{
-		$user = $this->application->getUser();
-		if (!$user) return null;        
-		$orders = $user->getOrders()->orderBy($this->getParam('order'), $this->getParam('sort'));        
-		if ($this->getParam('limit')) $orders->setItemCountPerPage($this->getParam('limit')); 
-		if ($this->getParam('where')) $orders->where($this->getParam('where'));         
-        $orders->setCurrentPageNumber( $this->getPage() );
-        return $orders;
-	}
+public function getOrders()
+{
+    $user = $this->application->getUser();
+    if (!$user) return null;        
+
+    $orders = $user->getOrders()->orderBy($this->getParam('order'), $this->getParam('sort'));        
+    if ($this->getParam('limit')) $orders->setItemCountPerPage($this->getParam('limit')); 
+    if ($this->getParam('where')) $orders->where($this->getParam('where'));         
+    $orders->setCurrentPageNumber($this->getPage());
+
+    $db = \Cetera\DbConnection::getDbConnection();
+
+    $orderIds = [];
+    foreach ($orders as $o) {
+        $orderIds[] = $o->id;
+    }
+    if (!empty($orderIds)) {
+        $placeholders = str_repeat('?,', count($orderIds) - 1) . '?';
+        $rows = $db->fetchAll("
+            SELECT order_id, product_id
+            FROM sale_order_products
+            WHERE order_id IN ($placeholders)
+            ORDER BY order_id, id
+        ", $orderIds);
+        $productIdsByOrder = [];
+        foreach ($rows as $row) {
+            $productIdsByOrder[$row['order_id']][] = $row['product_id'];
+        }
+        foreach ($orders as $order) {
+            $order->product_ids = $productIdsByOrder[$order->id] ?? [];
+        }
+    } else {
+        foreach ($orders as $order) {
+            $order->product_ids = [];
+        }
+    }
+
+    return $orders;
+}
 	
 }
